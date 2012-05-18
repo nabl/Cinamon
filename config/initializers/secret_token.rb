c6895246400eb62a4e78907f3ca64afac11665ea
T <git@nabl.de>
2012-05-18 05:38:28 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cinamon::Application.config.secret_token = '7d6eb35a6f0ee5cd6f34369afbd3d0a2e7e4746a6908c32bdd4829b52f920b15d96b488c18782d7a04f8d0501d994d00d0263e49e50286e7dea9e8fdde881c88'
